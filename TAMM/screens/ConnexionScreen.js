a20cda074c1d34a10c2e9d38202762010394bc35
zineb <zineb.laanaya@gmail.com>
2024-01-10 19:07:33 +0100

    mise en page du login

--- TAMM/screens/ConnexionScreen.js
+++ TAMM/screens/ConnexionScreen.js
@@ -1,13 +1,11 @@
-import { View, Text,Image } from 'react-native'
-import React from 'react'
+import { View, Image } from 'react-native';
+import React from 'react';
 
 export default function ConnexionScreen() {
   return (
     <View className="bg-gray-900 h-full w-full ">
-      
-    
-  </View>
+    <Image className="h-1/2 w-full absolute " source={require('../assets/Chef-pana.png')} />    
+    </View>
   )
 }
-
 
